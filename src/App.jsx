73e26b7d921b10b00648dfb8ed73557aab242d4f
Aakash <vermaaakash1000@gmail.com>
2023-03-26 19:24:57 +0530

    Header goes into router layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,19 @@ import About from './components/About'
 import Home from './components/Home'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Logog from './assets/logog.png';
+import VanDetail from './components/VanDetail'
 import "../server"
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <nav className='nav'>
-          <Link to="/"> <img style={{ height: '24px', width: '172px', marginLeft: '2rem' }} src={Logog} alt="logo" />  </Link>
-          <div className='items'>
-            <Link style={{ textDecoration: 'none', color: '#4D4D4D', fontSize: '22px' }} to="/">Home</Link>
-            <Link style={{ textDecoration: 'none', color: '#4D4D4D', fontSize: '22px' }} to="/about">About</Link>
-            <Link style={{ textDecoration: 'none', color: '#4D4D4D', fontSize: '22px' }} to="/vans">Vans</Link>
-          </div>
-
-        </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
         </Routes>
       </BrowserRouter>
     </>
