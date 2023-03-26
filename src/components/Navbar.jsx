b4c9577133826e508fd41663d0fb939f55c87852
Aakash <vermaaakash1000@gmail.com>
2023-03-26 19:25:33 +0530

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav className='nav'>
+            <Link to="/"> <img style={{ height: '24px', width: '172px', marginLeft: '2rem' }} src={Logog} alt="logo" />  </Link>
+            <div className='items'>
+                <Link style={{ textDecoration: 'none', color: '#4D4D4D', fontSize: '22px' }} to="/">Home</Link>
+                <Link style={{ textDecoration: 'none', color: '#4D4D4D', fontSize: '22px' }} to="/about">About</Link>
+                <Link style={{ textDecoration: 'none', color: '#4D4D4D', fontSize: '22px' }} to="/vans">Vans</Link>
+            </div>
+
+        </nav>
     )
 }
 
