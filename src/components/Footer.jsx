3df5c2073d83192af92ed15d80695c45c65e5379
Aakash <vermaaakash1000@gmail.com>
2023-04-01 09:35:16 +0530

    Footer page added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
 
     return (
-        <div className='footer'>cc 2022 #VANLIFE</div>
+        <div className='footer'>&#169; 2022 #VANLIFE</div>
     )
 }
 
