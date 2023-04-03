7add0788c5a2672b08c27ed0013c7303d2f9416c
Aakash <vermaaakash1000@gmail.com>
2023-04-03 22:02:43 +0530

    new Host Van and detail page imported

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from './components/Home'
 import About from './components/About'
 import Vans from './components/Vans/Vans'
 import VanDetail from './components/Vans/VanDetail'
+import HostVans from './components/Host/HostVans'
+import HostVansDetail from './components/Host/HostVansDetail'
 import Footer from './components/Footer'
 import Dashboard from './components/Host/Dashboard'
 import HostLayout from './components/Host/HostLayout'
@@ -10,7 +12,7 @@ import Income from './components/Host/Income'
 import Reviews from './components/Host/Reviews'
 import "../server"
 import Layout from './components/Layout'
-
+import HostVanDetailLayout from './components/Host/HostVanDetailLayout'
 
 function App() {
   return (
@@ -25,8 +27,12 @@ function App() {
 
             <Route path="/host" element={<HostLayout />}>
               <Route index element={<Dashboard />} />
-              <Route path='/host/income' element={<Income />} />
-              <Route path='/host/Reviews' element={<Reviews />} />
+              <Route path='income' element={<Income />} />
+              <Route path='Reviews' element={<Reviews />} />
+              <Route path='vans' element={<HostVans />} />
+              <Route path='vans/:id' element={<HostVanDetailLayout />} >
+
+              </Route>
             </Route>
 
 
