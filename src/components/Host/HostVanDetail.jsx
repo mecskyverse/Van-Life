a6fb2edeebdfe62d3700330a086f88286b4b3813
Aakash <vermaaakash1000@gmail.com>
2023-04-06 08:02:31 +0530

    Van description page added

---
+++ src/components/Host/HostVanDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink, useOutletContext } from 'react-router-dom'
+function HostVansDetail() {
+    const currentVan = useOutletContext();
+    console.log(currentVan);
+    return (
+        <>
+            <p><b>Name:</b> {currentVan.name}</p>
+            <p><b>Category:</b> {currentVan.type}</p>
+            <p><b>Description:</b> {currentVan.description}</p>
+            <p><b>Visibility:</b> public</p>
+
+        </>
+    )
+}
+
+export default HostVansDetail
