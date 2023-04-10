3e9e323d70d13fc043552b08888553504104ed13
Aakash <vermaaakash1000@gmail.com>
2023-04-10 21:04:26 +0530

    Van Dtail filter back option added

--- src/components/Vans/VanDetail.jsx
+++ src/components/Vans/VanDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useLocation, Link } from 'react-router-dom'
 function VanDetail() {
     const params = useParams();
     console.log(params);
@@ -10,19 +10,31 @@ function VanDetail() {
             .then(data => setVan(data.vans))
 
     }, [params.id])
+    const location = useLocation();
+    console.log(location)
+    //optional chaining new feature in js
+    const search = location.state?.search || "";
+    const type = location.state?.type || "all";
     return (
-        <div className="van-detail-container">
-            {van ? (
-                <div className="van-detail">
-                    <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                    <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>
-                </div>
-            ) : <h2>Loading...</h2>}
-        </div>
+        <>
+            <Link
+                to={`..${search}`}
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to {type} vans</span></Link>
+            <div className="van-detail-container">
+                {van ? (
+                    <div className="van-detail">
+                        <img src={van.imageUrl} />
+                        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                        <h2>{van.name}</h2>
+                        <p className="van-price"><span>${van.price}</span>/day</p>
+                        <p>{van.description}</p>
+                        <button className="link-button">Rent this van</button>
+                    </div>
+                ) : <h2>Loading...</h2>}
+            </div>
+        </>
 
     )
 
