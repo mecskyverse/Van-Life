6efd845fb9095780036bb245c15a832341650b2e
Aakash <vermaaakash1000@gmail.com>
2023-04-16 19:41:30 +0530

    Error case Inserted in api call

--- src/api.js
+++ src/api.js
@@ -1,5 +1,14 @@
-export async function getVans(){
-            const items = await fetch("/api/vans")
-            const json = await items.json();
-            return json.vans;
+export async function getVans(id){
+    const url = id ? `/api/vans/${id}`: '/api/vans' 
+
+    const res = await fetch(url)
+    if(!res.ok){
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const json = await res.json();
+    return json.vans;
 }
